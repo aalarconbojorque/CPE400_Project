85f735d48933912c2b75faf0cfe04ac2c435b70a
Andy <aa.alarcon10@gmail.com>
2021-11-08 09:27:16 -0800

    Added hello world

---
+++ proj.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------------------------
+# FILE NAME:         proj.py
+# USAGE:             python3 proj3.py
+# NOTES:             Requires Python3                    
+# Authors:           Andy Alarcon and Griffin Wagenknecht
+# -----------------------------------------------------------------------------
+
+import numpy as np
+
+def main():
+    
+    print("Hello World!")
+    
+
+    
+
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     CompMagSqr()
+# PURPOSE:           Computes the magnitude squared
+# -----------------------------------------------------------------------------
+
+    
+if __name__ == "__main__":
+    main()
