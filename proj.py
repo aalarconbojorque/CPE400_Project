a60529371beef8d868a5070b852f2a75972f462a
Andy <aa.alarcon10@gmail.com>
2021-11-08 17:44:25 -0800

    Imported networkx libary test case

--- proj.py
+++ proj.py
@@ -5,11 +5,35 @@
 # Authors:           Andy Alarcon and Griffin Wagenknecht
 # -----------------------------------------------------------------------------
 
-import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def main():
     
     print("Hello World!")
+    G = nx.Graph()
+    G.add_node(1)
+    G.add_node(2)
+    G.add_node(3)
+    G.add_node(4)
+    G.add_node(5)
+    G.add_edge(1, 2)
+    G.add_edge(2, 3)
+    G.add_edge(3, 4)
+    G.add_edge(1, 5)
+    G.add_edge(5, 4)
+    subax1 = plt.subplot(121)
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
+    print("Shortest path before removing Node 5 from 1 -> 4")
+    print(nx.shortest_path(G, source=1, target=4))
+    G.remove_node(5)
+    print("Shortest path after removing Node 5 from 1 -> 4")
+    print(nx.shortest_path(G, source=1, target=4))
+   
+
+
+
     
 
     
