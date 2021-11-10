ee373ee846bb219752687c0bdb64ef69757bddda
Andy <aa.alarcon10@gmail.com>
2021-11-09 19:36:01 -0800

    Built main loop menu

--- proj.py
+++ proj.py
@@ -25,6 +25,44 @@ def main():
     else:
         sys.exit()
 
+    print("-------------------------------------------------------------")
+    print("Dynamic routing mechanism design in a faulty network Simulation")
+    
+    #Main menu loop
+    while(True):
+        print("-------------------------------------------------------------")
+        print("Menu Options : ")
+        print("[1] - Select a source and destination route to experiment")
+        print("[2] - View the input network")
+        print("[3] - Exit")
+        option = ''
+        try:
+            option = int(input('Enter your selection : '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        if option == 1:
+            print("Option 1")
+        elif option == 2:
+             DisplayGraph(G)
+        elif option == 3:
+            print('Thank you !')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
+    
+    
+
+    
+
+
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     DisplayGraph(G)
+# PURPOSE:           Displays a window graph 
+# -----------------------------------------------------------------------------
+
+
+def DisplayGraph(G):
+    
     # Setup graph
     pos = nx.circular_layout(G)
     subax1 = plt.subplot(121)
@@ -39,57 +77,7 @@ def main():
                             font_color='black',  font_size=10, font_weight='bold')
     ax = plt.gca()
     ax.margins(0.2)
-
-    # Plot shortest path and graph
-    # path = nx.shortest_path(G, source=1, target=4, weight="failure")
-    # path_edges = zip(path, path[1:])
-    # path_edges = set(path_edges)
-    # nx.draw_networkx_nodes(G, pos, nodelist=path,
-    #                        node_color='r', node_size=2000)
-    # nx.draw_networkx_edges(G, pos, edgelist=path_edges,
-    #                        edge_color='r', width=5)
-
     plt.show()
-
-    # plt.clf()
-
-    # #Remove nodes
-    # H = G.copy()
-    # remove = []
-    # for node in H.nodes():
-    #     if H.nodes[node]['failure'] >= 100:
-    #         remove.append(node)
-    #     else:
-    #         pass
-    # G.remove_nodes_from(remove)
-
-    # # Setup graph
-    # pos = nx.circular_layout(G)
-    # subax1 = plt.subplot(121)
-    # nx.draw(G, pos, font_weight='bold', node_size=2000)
-    # edge_labels = nx.get_edge_attributes(G, 'failure')
-    # node_labels = nx.get_node_attributes(G, 'failure')
-    # for key, value in node_labels.items():
-    #     node_labels[key] = "[" + str(key) + "," + str(value) + "]"
-    # nx.draw_networkx_edge_labels(
-    #     G, pos, edge_labels=edge_labels, font_color='black', font_weight='bold')
-    # nx.draw_networkx_labels(G, pos, labels=node_labels,
-    #                         font_color='black',  font_size=10, font_weight='bold')
-    # ax = plt.gca()
-    # ax.margins(0.2)
-
-    # # Plot shortest path and graph
-    # path = nx.shortest_path(G, source=1, target=4, weight="failure")
-    # path_edges = zip(path, path[1:])
-    # path_edges = set(path_edges)
-    # nx.draw_networkx_nodes(G, pos, nodelist=path,
-    #                        node_color='r', node_size=2000)
-    # nx.draw_networkx_edges(G, pos, edgelist=path_edges,
-    #                        edge_color='r', width=5)
-
-    # plt.draw()
-    # plt.pause(2)
-
 
 # ----------------------------------------------------------------------------
 # FUNCTION NAME:     ObtainNodeData(G)
